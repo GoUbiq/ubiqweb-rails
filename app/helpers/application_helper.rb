62a184f73f868d94883e89d8b342aa257e4be6a6
Jayanth Kottapalli <jayanth1991@hotmail.com>
2014-06-15 00:16:46 -0400

    layout is done HOWEVER the css is a clusterfuck having too many issues with rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns the full title based on pages
+	def full_title(page_title)
+		base_title = "Ubiq"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} - #{page_title}"
+		end
+	end
 end
--- spec/requests/indices_spec.rb
+++ spec/requests/indices_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "Indices" do
 
+	before { visit root_path }
+
 	describe "Home page" do
-		it "should have the content 'Ubiq'" do
-			visit '/index/home'
+		it "should have the content 'Ubiq'" do			
 			expect(page).to have_content('Ubiq')
 		end
 
-		it "shoudl have the title 'Ubiq'" do
-			visit '/index/home'
+		it "shoudl have the title 'Ubiq'" do			
 			expect(page).to have_title('Ubiq - Wireless Presentations')
 		end
 	end
